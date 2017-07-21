058493e595b217dfc379bdd09f4eda0a72051cbf
Hack Reactor Student <student@hackreactor.com>
2017-07-20 19:19:17 -0500

    Completes section 02 of the tutorial, adding transpilation, linting, type annotation, testing, test hooks

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// @flow
+/* eslint-disable no-console */
+
+// const Dog = require('./dog')
+import Dog from './dog'
+
+console.log('hello world')
+
+const str = 'ES6'
+console.log(`Hello ${str}`)
+
+const toby = new Dog('Toby')
+
+console.log(toby.bark())
